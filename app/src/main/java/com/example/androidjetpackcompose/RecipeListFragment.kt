6242dae31129d3ca2e33ff0d1d852fb4f0a07240
ilyosbekibrohimov <ilyos199822@gmail.com>
2021-02-05 12:11:37 +0900

    RecipeFragment added

--- app/src/main/java/com/example/androidjetpackcompose/RecipeListFragment.kt
+++ app/src/main/java/com/example/androidjetpackcompose/RecipeListFragment.kt
@@ -5,20 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 
 class RecipeListFragment : Fragment() {
@@ -28,34 +32,17 @@ class RecipeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.recipe_list, container, false)
-        view.findViewById<ComposeView>(R.id.compose_view).setContent {
-
-            Column(
-                modifier = Modifier
-                    .border(border = BorderStroke(1.dp, color = Color.Black))
-                    .padding(16.dp)
-            ) {
-                androidx.compose.material.Text(text = "This is an introduction to ComposeView  in Jetpack")
-                Spacer(modifier = Modifier.padding(10.dp))
-                CircularProgressIndicator()
-                Spacer(modifier = Modifier.padding(10.dp))
-                androidx.compose.material.Text(text = "NEAT")
-                Spacer(modifier = Modifier.padding(10.dp))
-
-                val customView = HorizontalDottedProgress(AmbientContext.current)
-                AndroidView(viewBlock = { customView })
-
-                val customView2 = inflater.inflate(R.layout.button, container, false)
-                AndroidView(viewBlock = { customView2 })
-
-
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Recipe List", style = TextStyle(fontSize = 21.sp))
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = { findNavController().navigate(R.id.viewRecipe) }) {
+                        Text(text = "Recipe List", style = TextStyle(fontSize = 21.sp))
+                    }
+                }
             }
-
-
         }
-
-        return view
     }
 }
 
